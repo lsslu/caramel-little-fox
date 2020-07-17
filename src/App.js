665a69958e1ba59c103790211715eec0abc55729
lss <lsslu@hotmail.com>
2020-07-18 01:10:11 +0800

    delete fan ranking

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import { Store } from './store'
 import Info from './components/Info';
-import FanRanking from './components/FanRanking';
 import defaultData from './store/defaultData';
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
     <Store.Provider value={defaultData}>
       <div className="App">
         <Info />
-        <FanRanking />
       </div>
     </Store.Provider>
   );
